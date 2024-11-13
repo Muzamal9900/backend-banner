fa1ac77dfa2719adda0690821be0473bb1856e4f
Rana Muzamil <mmlrana00@gmail.com>
2024-11-13 07:57:32 -0800

    ix issues

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -30,12 +30,28 @@ export const loginUser = async (req, res) => {
   if (!isPasswordValid)
     return res.status(401).json({ message: "Invalid email or password" });
 
+  const orderWithPayment = await prisma.order.findFirst({
+    where: {
+      userId: user.id,
+      payment: {
+        some: {
+          status: "paid",
+        },
+      },
+    },
+  });
+
+  const orderAvail = orderWithPayment ? true : false;
+
   const token = jwt.sign(
-    { id: user.id, email: user.email, name: user.name },
-    process.env.JWT_SECRET,
     {
-      expiresIn: "1h",
-    }
+      id: user.id,
+      email: user.email,
+      name: user.name,
+      orderAvail: orderAvail,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "1h" }
   );
 
   res.json({
